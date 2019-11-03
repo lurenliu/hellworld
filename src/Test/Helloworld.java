b908899dbdd9141c242c3ca6c557ebe3a1eb129b
lurenliu <32644374+lurenliu@users.noreply.github.com>
2019-11-03 09:10:49 +0800

    test  utf-8

--- src/Test/Helloworld.java
+++ src/Test/Helloworld.java
@@ -3,7 +3,7 @@ package Test;
 public class Helloworld {
 
 	public static void main(String[] args) {
-		System.out.println("hello  world by github new随便动一动  改点东西");
+		System.out.println("hello  world by github new Test 中文编码");
 	}
 	
 	
