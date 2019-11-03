e04fb2d84f68f9c8392d574779bd5a577b6c6078
lurenliu <32644374+lurenliu@users.noreply.github.com>
2019-11-03 09:06:22 +0800

    new test

--- src/Test/Helloworld.java
+++ src/Test/Helloworld.java
@@ -3,7 +3,7 @@ package Test;
 public class Helloworld {
 
 	public static void main(String[] args) {
-		System.out.println("hello  world by github");
+		System.out.println("hello  world by github 随便动一动  改点东西");
 	}
 	
 }
